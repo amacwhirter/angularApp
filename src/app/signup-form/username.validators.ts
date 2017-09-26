1960d1138330b243f12149e3b0d9c331d420f4d1
Ashley MacWhirter <macwhirtera@gmail.com>
2017-09-26 18:12:30 -0400

    adding async validators and load pending

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -4,13 +4,17 @@ export class UsernameValidators {
   static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
     if ((control.value as string).indexOf(' ') != -1)
       return { cannotContainSpace: true };
-
       return null;
   }
 
-  static shouldBeUnique(control: AbstractControl) : ValidationErrors | null {
-    if ((control.value === 'mosh')
-      return { shouldBeUnique : true };
-    return null;
+  static shouldBeUnique(control: AbstractControl) : Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'mosh')
+          resolve({ shouldBeUnique : true });
+        else
+          resolve(null);
+      }, 2000);
+    });
   }
 }
