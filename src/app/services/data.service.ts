fe1a8bc47f6bbf6b80cc0a620f36fbf9530636c3
Ashley MacWhirter <macwhirtera@gmail.com>
2017-09-28 15:32:40 -0400

    switchmap operator

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -17,7 +17,7 @@ export class DataService {
   getAll(){
     return this.http.get(this.url)
       .map(response => response.json())
-      .catch(this.handleError);
+      // .catch(this.handleError);
   }
 
   create(resource){
